3127a5172606886b69d92e9e1cbfc855478dcb42
euoluizg <gmelo17177@gmail.com>
2026-03-07 00:11:06 -0300

    Add pages components

---
+++ ImpactaMais/src/app/pages/send-code-page/send-code-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-code-page',
+  imports: [],
+  templateUrl: './send-code-page.html',
+  styleUrl: './send-code-page.scss',
+})
+export class SendCodePage {
+
+}
